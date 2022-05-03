d117172ff3728ff797e673f5b303c772df3fa24d
horanDEV <morozovvv2005@gmail.com>
2022-05-03 17:15:08 +0700

    about:black

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,6 @@
 <template>
-  <div class="flex flex-col bg-slate-500 w-screen h-screen font-sans">
-    <header class="flex justify-center h-12 bg-slate-700 text-white">
-      <ul class="flex h-full items-center gap-5 theader">
-        <router-link to="/home"> Home </router-link>
-        <router-link to="/first"> First Page </router-link>
-        <router-link to="/second"> Second Page </router-link>
-      </ul>
-    </header>
-    <div class="flex-auto">
-      <router-view />
-    </div>
+  <div class="w-screen h-screen bg-slate-400">
+    <router-view />
   </div>
 </template>
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import first from "../pages/first.vue";
-import second from "../pages/second.vue";
 import notFound from "../pages/404.vue";
 import home from "../pages/home.vue";
 
 const routes = [
   {
-    path: "/home",
+    path: "/",
     name: "home",
     component: home,
   },
   {
-    path: "/first",
-    name: "first",
-    component: first,
-  },
-  {
-    path: "/second",
-    name: "second",
-    component: second,
-  },
-  {
-    path: "/",
+    path: "/:catchAll(.*)",
     name: "notFound",
     component: notFound,
   },
